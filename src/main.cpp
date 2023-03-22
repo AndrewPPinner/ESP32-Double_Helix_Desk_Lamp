4074f6aebb3dc655253def1e335af401c3b64d63
AndrewPPinner <andppinner@gmail.com>
2023-03-22 15:19:15 -0400

    Adding OTA updates

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include <AsyncTCP.h>
+#include <AsyncElegantOTA.h>
 
 #include <html.h>
 #include <js.h>
@@ -18,13 +19,17 @@ int r = 182;
 int g = 24;
 int b = 226;
 bool spiralAction = false;
+bool spiralRandomAction = false;
+bool dotAction = false;
 
 void main_Page(AsyncWebServerRequest *request) {
   request->send(200, "text/html", HTML() + JS());
 }
 
 void primary_LED(AsyncWebServerRequest *request){
+  spiralRandomAction = false;
   spiralAction = false;
+  dotAction = false;
   for (int i = 0; i < LED_COUNT; i++)
   {
     strip.setPixelColor(i, strip.Color(request->getParam("r")->value().toInt(), request->getParam("g")->value().toInt(), request->getParam("b")->value().toInt()));
@@ -38,6 +43,8 @@ void primary_LED(AsyncWebServerRequest *request){
 
 void random_LED(AsyncWebServerRequest *request){
   spiralAction = false;
+  spiralRandomAction = false;
+  dotAction = false;
   for (int i = 0; i < LED_COUNT; i++)
   {
     strip.setPixelColor(i, strip.Color(random(0,255), random(0,255), random(0,255)));
@@ -46,13 +53,24 @@ void random_LED(AsyncWebServerRequest *request){
   request->send(200);
 }
 
+void random_spiral_LED(AsyncWebServerRequest *request){
+  spiralAction = false;
+  spiralRandomAction = true;
+  dotAction = false;
+  request->send(200);
+}
+
 void spiral_LED(AsyncWebServerRequest *request){
+  spiralRandomAction = false;
   spiralAction = true;
+  dotAction = false;
   request->send(200);
 }
 
 void xmas_LED(AsyncWebServerRequest *request){
+  spiralRandomAction = false;
   spiralAction = false;
+  dotAction = false;
   for (int i = 0; i < 100; i++)
   {
     strip.setPixelColor(i, strip.Color(30, 255, 27));
@@ -72,28 +90,46 @@ void xmas_LED(AsyncWebServerRequest *request){
 }
 
 void clear_LED(AsyncWebServerRequest *request){
+  spiralRandomAction = false;
   spiralAction = false;
+  dotAction = false;
   strip.show();
   strip.clear();
   request->send(200);
 }
 
+void LED_brightness(AsyncWebServerRequest *request){
+  strip.setBrightness(request->getParam("b")->value().toInt());
+  strip.show();
+  request->send(200, "text/html", String(strip.getBrightness()));
+}
+
+void dot_LED(AsyncWebServerRequest *request){
+  dotAction = true;
+  request->send(200);
+}
+
 void setup() {
   WiFi.begin(ssid, password);
+  Serial.begin(115200);
   server.on("/", HTTP_GET, [] (AsyncWebServerRequest *request){main_Page(request);});
   server.on("/primary", HTTP_GET, [] (AsyncWebServerRequest *request){primary_LED(request);});
   server.on("/random", HTTP_GET, [] (AsyncWebServerRequest *request){random_LED(request);});
+  server.on("/random_spiral", HTTP_GET, [] (AsyncWebServerRequest *request){random_spiral_LED(request);});
   server.on("/spiral",  HTTP_GET, [] (AsyncWebServerRequest *request){spiral_LED(request);});
   server.on("/xmas",  HTTP_GET, [] (AsyncWebServerRequest *request){xmas_LED(request);});
   server.on("/clear",  HTTP_GET, [] (AsyncWebServerRequest *request){clear_LED(request);});
+  server.on("/brightness",  HTTP_GET, [] (AsyncWebServerRequest *request){LED_brightness(request);});
+  server.on("/dot",  HTTP_GET, [] (AsyncWebServerRequest *request){dot_LED(request);});
 
-
+  AsyncElegantOTA.begin(&server, &strip);
   server.begin();
 
   strip.begin();
   strip.show();
   strip.clear();
   strip.setBrightness(50);
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
@@ -108,6 +144,31 @@ void loop() {
     {
       strip.setPixelColor(i, strip.Color(0, 0, 0));
       strip.show();
+    }
+  }
+  if (spiralRandomAction) {
+    for (int i = 0; i < LED_COUNT; i++)
+    {
+      strip.setPixelColor(i, strip.Color(random(0,255), random(0,255), random(0,255)));
+      strip.show();
+    }
+    delay(10);
+    for (int i = LED_COUNT; i > 0; i--)
+    {
+      strip.setPixelColor(i, strip.Color(random(0,255), random(0,255), random(0,255)));
+      strip.show();
+    }
+  }
+  if (dotAction) {
+    for (int i = 0; i < LED_COUNT; i++)
+    {
+      strip.setPixelColor(i, strip.Color(r, g, b));
+      strip.setPixelColor(i + 1, strip.Color(r, g, b));
+      strip.setPixelColor(i + 2, strip.Color(r, g, b));
+      strip.setPixelColor(i + 3, strip.Color(r, g, b));
+      strip.setPixelColor(i - 1, strip.Color(0, 0, 0));
+      strip.show();
+      delay(10);
     }
   }
 }
